1f68c1636e9e754c4ac96274337bd89469ff204c
Mercen <mercen@mercen.net>
2023-10-16 20:57:39 +0900

    ADD: ResultBuilder Optional

--- Sources/OpenTDS/Component/Button/Toolbar/ToolbarButton+Ext.swift
+++ Sources/OpenTDS/Component/Button/Toolbar/ToolbarButton+Ext.swift
@@ -2,7 +2,12 @@ import SwiftUI
 
 @resultBuilder
 public struct TossToolbarButtonBuilder {
+    
     public static func buildBlock(_ components: TossToolbarButton...) -> [TossToolbarButton] {
         components
+    }
+    
+    static func buildOptional(_ component: TossToolbarButton?) -> TossToolbarButton? {
+        return component ?? nil
     }
 }
